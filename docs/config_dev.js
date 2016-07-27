207605783d1f69c6fa65f2ae144d2ca1e531d646
zengweigang <zengweigang@gmail.com>
2016-07-27 10:29:35 +0800

    登陆地址修改

--- docs/config_dev.js
+++ docs/config_dev.js
@@ -2,6 +2,6 @@ var URLS = {
     USER_BASE: "https://user-dev.36b.me",
     GAME_INFO_BASE: "https://gameinfo-dev.36b.me",
     MISC_BASE: "https://misc-dev.36b.me",
-    WWW_BASE: "https://www.36b.me",
+    WWW_BASE: "https://user.36b.me",
     FILE_BASE: "http://o850rkfpc.bkt.clouddn.com"
 };
