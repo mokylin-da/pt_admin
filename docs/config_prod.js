763c6a46f447d9ba9b3b09ded3c27a1c2b0052ea
zengweigang <zengweigang@gmail.com>
2016-07-27 10:22:38 +0800

    登陆地址修改

--- docs/config_prod.js
+++ docs/config_prod.js
@@ -2,5 +2,6 @@ var URLS = {
     USER_BASE: "https://userapi.mlyx.com",
     GAME_INFO_BASE: "https://gameapi.mlyx.com",
     MISC_BASE: "https://miscapi.mlyx.com",
+    WWW_BASE: "https://www.mlyx.com",
     FILE_BASE: "http://o8508ib3h.bkt.clouddn.com"
 };
