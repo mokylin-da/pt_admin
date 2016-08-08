8eb7d8494d58f6b2a362698b52626cf53c0673ac
zengweigang <zengweigang@gmail.com>
2016-08-08 15:09:40 +0800

    序号 以及正式的文件域名修改

--- docs/config_prod.js
+++ docs/config_prod.js
@@ -3,6 +3,5 @@ var URLS = {
     GAME_INFO_BASE: "https://gameapi.mlyx.com",
     MISC_BASE: "https://miscapi.mlyx.com",
     WWW_BASE: "http://user.mlyx.com",
-    FILE_BASE: "http://o8508ib3h.bkt.clouddn.com"
     FILE_BASE: "http://file.mlyx.com"
 };
