aebce7d139552546de265769059aefdce339ff15
zengweigang <zengweigang@gmail.com>
2016-09-02 14:24:24 +0800

    增加13游戏的配置构建

---
+++ docs/config_prod_13yx.js
@@ -0,0 +1,8 @@
+var URLS = {
+    USER_BASE: "https://userapi.13yx.com",
+    GAME_INFO_BASE: "https://gameapi.13yx.com",
+    MISC_BASE: "https://miscapi.13yx.com",
+    PAY_BASE: "https://payapi.13yx.com",
+    WWW_BASE: "http://user.13yx.com",
+    FILE_BASE: "http://file.13yx.com"
+};
